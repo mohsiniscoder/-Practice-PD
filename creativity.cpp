39b15056f0251154f9d0acf0758555a9797303c3
mohsiniscoder <96002185+mohsiniscoder@users.noreply.github.com>
2022-12-15 22:55:26 +0500

    Add files via upload

---
+++ creativity.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<" ____________________________________               "<< endl;
+cout<<"|                                    |              "<< endl;
+cout<<"|        ////////////////////////    |              "<< endl;
+cout<<"|                                    |              "<< endl;
+cout<<"|         ___________________________|              "<< endl;
+cout<<"|        | #     |                                  "<< endl;
+cout<<"|        |  #    )                                  "<< endl;
+cout<<"|        |   #  )                                   "<< endl;
+cout<<"|        |     )                                    "<< endl;
+cout<<"|        |-----                                     "<< endl;
+cout<<"|        |                                          "<< endl;
+cout<<"|________|                                          "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+cout<<"                                                    "<< endl;
+}
