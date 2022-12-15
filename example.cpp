04e1999a7fd9b91952d48f4b8578439b958358bf
mohsiniscoder <96002185+mohsiniscoder@users.noreply.github.com>
2022-12-15 22:16:14 +0500

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<< "This is My First Program in the university" << endl;
+cout<<"My Roll Number is 123"<< endl;
+cout<<"I am 18 years Old";
+
+
+
+}
