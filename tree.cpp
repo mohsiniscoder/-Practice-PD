99c0e24572c29ffd6ee479f75e765ef6b2b86185
mohsiniscoder <96002185+mohsiniscoder@users.noreply.github.com>
2022-12-18 18:13:34 +0500

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    system("color 72");
+    cout << "                        *                                      " << endl;
+    cout << "                       ***                                       " << endl;
+    cout << "                      *****                                         " << endl;
+    cout << "                     *******                                          " << endl;
+    cout << "                    *********                                           " << endl;
+    cout << "                   ***********                                            " << endl;
+    cout << "                  *************                                               " << endl;
+    cout << "                 ***************                                              " << endl;
+    cout << "                       ##                                       " << endl;
+    cout << "                       ##                                        " << endl;
+    cout << "                       ##                                        " << endl;
+    cout << "                       ##                                       " << endl;
+}
