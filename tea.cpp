ada4dbe18b79449a153baa8f53fc88cfb7067dd2
mohsiniscoder <96002185+mohsiniscoder@users.noreply.github.com>
2022-12-18 17:36:39 +0500

    Add files via upload

---
+++ tea.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+main()
+{   
+system("color 76 ");
+cout<<"     *            *            *                               "<<endl;
+cout<<"       *            *            *                               "<<endl;
+cout<<"         *            *            *                            "<<endl;
+cout<<"        *            *            *                            "<<endl;
+cout<<"       *            *            *                            "<<endl;
+cout<<"        *             *            *                          "<<endl;
+cout<<"         *             *             *                        "<<endl;
+cout<<"   ******************************************########                                                      "<<endl;
+cout<<"    *                                      *         #      "<<endl;
+cout<<"      *                                   *          #       "<<endl;
+cout<<"        *                                *           #      "<<endl;
+cout<<"          *                             ****#########        "<<endl;
+cout<<"            *                          *                  "<<endl;
+cout<<"              *                       *                    "<<endl;
+cout<<"                * ********************                                        "<<endl;
+cout<<"                                                            "<<endl;
+cout<<"                                                           "<<endl;
+cout<<"                                                            "<<endl;
+cout<<"                                                           "<<endl;
+cout<<"                                                            "<<endl;
+cout<<"                                                           "<<endl;
+cout<<"                                                            "<<endl;
+
+
+}
