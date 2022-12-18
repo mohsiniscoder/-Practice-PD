11fb6d489ab03a3ac5ca5adc7d7d27c35953ff87
mohsiniscoder <96002185+mohsiniscoder@users.noreply.github.com>
2022-12-18 17:27:53 +0500

    Add files via upload

---
+++ car.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"        _____________________                                      "<<endl;
+cout<<"      //                     \\                   "<<endl;
+cout<<"     (      My Car :P         )                "<<endl;
+cout<<"      -   _                 _ /                   "<<endl;
+cout<<"    == --(_)---------------(_)                                      "<<endl;
+cout<<"                                              "<<endl;
+cout<<"                                              "<<endl;
+
+}
